bce17c57f4d8380c13c40737662ad52f56288c5a
李波 <libo01@51talk.com>
2019-01-14 15:26:48 +0800

    websocket

--- Applications/SmartCRM/start_gateway.php
+++ Applications/SmartCRM/start_gateway.php
@@ -21,7 +21,7 @@ use \Workerman\Autoloader;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 // gateway 进程，这里使用Text协议，可以用telnet测试
-$gateway = new Gateway("webscoket://0.0.0.0:8282");
+$gateway = new Gateway("websocket://0.0.0.0:8282");
 // gateway名称，status方便查看
 $gateway->name = 'SmartCRMGateway';
 // gateway进程数
